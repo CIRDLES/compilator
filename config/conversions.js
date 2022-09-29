0ba72df68d48ea61b618a3739578e675fd4a2023
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-29 13:51:02 +0800

    Delete config directory

--- config/conversions.js
+++
@@ -1,7 +0,0 @@
-exports.clickOn = function (options) {
-	var output = "swinger.clickOn(matchingAll(";
-	Object.keys(options.selectors).forEach(function(key) {
-				output += '"' + key + ':' + options.selectors[key] + '", '
-	})
-	return output.slice(0, output.length - 2) + ");";
-}
--- config/preprocessor.js
+++
@@ -1,7 +0,0 @@
-exports.clickOnChildren = function(options) {
-	var output = "$..children[?(";
-	Object.keys(options.selectors).forEach(function(key) {
-				output += '@.' + key + '=="' + options.selectors[key] + '" && '
-	})
-	return output.slice(0, output.length - 4) + ")]";
-}
